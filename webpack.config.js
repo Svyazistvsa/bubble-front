df6f4b85b748cc197d240607329d250c4eaee7da
Svyazistvsa <sviazist.vsa@gmail.com>
2024-04-09 16:48:43 +0500

    feat: html and bubel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+    
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+            filename: '[name].bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Bubble',
+            template: path.resolve(__dirname,'./src/index.html'),
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            }
+        ]
+    }
+}
