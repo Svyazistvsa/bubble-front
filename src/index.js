596de549e886b3b30ea15bec28184e263fb8692c
Svyazistvsa <sviazist.vsa@gmail.com>
2024-04-13 14:50:49 +0500

    feat: To added the main part html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import trees from './images/trees.jpg'
-import gene from './images/gene.svg'
+
 import './styles/main.scss'
 
-const heading = document.createElement('h1')
-heading.textContent = 'Как интересно!'
-
-// добавляем заголовок в DOM
-const root = document.querySelector('#root')
-root.append(heading)
