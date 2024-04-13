8017242a05ffd3ac65b929c4f6fdd3813fa95a4d
Svyazistvsa <sviazist.vsa@gmail.com>
2024-04-13 14:19:44 +0500

    feat: The beginning of the design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import trees from './images/trees.jpg'
 import gene from './images/gene.svg'
+import './styles/main.scss'
 
 const heading = document.createElement('h1')
 heading.textContent = 'Как интересно!'
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
     
@@ -10,6 +11,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
             filename: '[name].bundle.js',
+    },
+    devServer: {
+        historyApiFallback: true,
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        open: true,
+        compress: true,
+        hot: true,
+        port: 9000,
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -25,7 +36,19 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ['babel-loader'],
-            }
-        ]
-    }
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ],
+    },
 }
