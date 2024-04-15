2ec2141671222128ca7710903fa41b58bad41911
Svyazistvsa <sviazist.vsa@gmail.com>
2024-04-15 21:02:34 +0500

    feat: start bubble generation

--- src/scripts/bubble-generator.js
+++ src/scripts/bubble-generator.js
@@ -1,9 +1,33 @@
 let screen = document.querySelector('#screen'),
-    ww;
+    ww,wh;
 
 window.addEventListener("resize", winWidth);
 
 function winWidth(){
     ww = window.innerWidth;
+    wh = window.innerHeight;
 }
 
+function generator(ww, wh){
+    let div = document.createElement('div'),
+        bottomPoint = getRandomInt(50, (ww - 50)),
+        diametr = getRandomInt(10, 300);
+    div.classList.add('backB');
+    div.style.cssText += `
+        left: ${bottomPoint}px;
+        width: ${diametr}px;
+        height: ${diametr}px;
+    `;
+    screen.append(div);
+}
+
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  
+  let timerId = setInterval(generator, 500, ww, wh);
