7bd1bb399e7e72ac91e0243ab0683de41923e36f
Svyazist <sviazist.vsa@gmail.com>
2024-11-12 22:03:55 +0500

    end

--- src/scripts/content_req.js
+++ src/scripts/content_req.js
@@ -1,9 +1,16 @@
 "use strict"
 
 let contentBox = document.querySelector('main');
-let run = async () => {let response = await fetch('https://localhost:3000/');
-
-let content = await response.json();
-contentBox.innerHTML = content;}
-
+let run = async () => {
+    try {
+        let response = await fetch('https://localhost:3000/');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let content = await response.text();
+        contentBox.innerHTML = `<p>${content}</p>`;
+    } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+    }
+}
 run();
